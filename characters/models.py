a041884f2ccb8b6ce0df7bcae7161da7ebefe77b
Andriy Zhuk <andri195595@gmail.com>
2024-11-04 16:15:59 +0200

    Added models and migrations

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Characters(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        ALIVE = "Alive", "Alive"
+        DEAD = "Dead", "Dead"
+        UNKNOWN = "Unknown", "Unknown"
+
+    class GenderChoices(models.TextChoices):
+        MALE = "Male", "Male"
+        FEMALE = "Female", "Female"
+        GENDERLESS = "Genderless", "Genderless"
+        UNKNOWN = "Unknown", "Unknown"
+
+    api_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, choices=StatusChoices.choices)
+    species = models.CharField(max_length=255)
+    gender = models.CharField(max_length=50, choices=GenderChoices.choices)
+    image = models.URLField(max_length=255, null=True)
+
+    def __str__(self) -> str:
+        return self.name
